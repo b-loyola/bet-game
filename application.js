3277d668962bfcae5723bd4f54344696d184f00c
Ben Silva <berna.loyola@gmail.com>
2016-05-02 14:21:22 -0700

    refactoring to use jQuery and the DOM

--- application.js
+++ application.js
@@ -1,45 +1,80 @@
 var money = 100;
-var result = "";
+var betAmt = 0;
+var betNum = 0;
+var message = "";
+var chosen = "";
 const MIN_BET = 5;
 const MAX_BET = 10;
 
-function promptForBet() {
-	return parseInt(window.prompt("You have: $" + money + "\nHow much do you want to bet? (min: " + MIN_BET + ", max: " + MAX_BET + ")"), 10);
+function getBet() {
+	return parseInt($('#bet').val(),10);
 }
 
-function promptForNumber() {
-	return parseInt(window.prompt("Enter a number between 1 and 10."), 10);
+function getNumber() {
+	return parseInt($('#number').val(),10);
 }
 
-while(money > MIN_BET) {
+function invalidBet(bet) {
+	console.log("inside invalidBet");
+	return (isNaN(bet) || bet < MIN_BET || bet > MAX_BET || bet > money);
+}
+
+function invalidNum(num) {
+	return (isNaN(num) || num < 1 || num > 10);
+}
+
+$(document).ready(function(){
+
+	// clear fields on focus
+	$('#input').find('input').focus(function(){
+		$(this).val('');
+	});
 
 	// get bet amount
-	do {
-		var bet = promptForBet();
-	} while (isNaN(bet) || bet < MIN_BET || bet > MAX_BET || bet > money);
+	$('#bet').keyup(function(){
+		betAmt = getBet();
+		if (invalidBet(betAmt)) {
+			$('#message').text("Bet must be a number between " + MIN_BET + " and " + MAX_BET);
+		} else {
+			$('#currentBet').text(betAmt);
+		}
+	});
 
 	// get bet number
-	do {
-		var selectedNumber = ;
-	} while (isNaN(selectedNumber) || selectedNumber < 1 || selectedNumber > 10);
+	$('#number').keyup(function(){
+		betNum = getNumber();
+		if (invalidNum(betNum)) {
+			$('#message').text("Number must be an integer between 1 and 10.");
+		} else {
+			$('#currentNumber').text(betNum);
+		}
+	});
 
-	// select random number between 1 and 10
-	var randomNumber = Math.ceil(Math.random() * 10);
+	//play game
+	$('#play').on('click', function() {
+		if (invalidBet(betAmt) || invalidNum(betNum)) {
 
-	switch (selectedNumber){
-		case randomNumber:
-			money += bet;
-			result = "You win!\nYour winnings: $" + bet + "\nTotal cash remaining: $" + money;
-			break;
-		case randomNumber + 1:
-		case randomNumber - 1:
-			result = "Close enough!\nYou don't win anything but you get to keep your bet.\nTotal cash remaining: $" + money;
-			break;
-		default:
-			money -= bet;
-			result = "You lose!\nTotal cash remaining: $" + money;
-	}
-	window.alert("I chose " + randomNumber + "\n" + result);
-}
+		}
+		var randomNumber = Math.ceil(Math.random() * 10);
+		chosen = "I chose " + randomNumber;
+		switch (betNum){
+			case randomNumber:
+				money += betAmt;
+				message = "You win! Your winnings: $" + betAmt;
+				break;
+			case randomNumber + 1:
+			case randomNumber - 1:
+				message = "Close enough! You don't win anything but you get to keep your bet.";
+				break;
+			default:
+				money -= betAmt;
+				message = "You lose!";
+		}
+		$('#bankroll').text("$" + money);
+		$('#chosen').text(chosen);
+		$('#message').text(message);
+	});
 
-window.alert("Game over!\nNot enough money to make bets\nCash remaining: $" + money);
+	// window.alert("Game over!\nNot enough money to make bets\nCash remaining: $" + money);
+
+});
